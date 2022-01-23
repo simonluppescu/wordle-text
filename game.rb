887b5f37aedfb0711cf101643082f58c9fe54c00
Simon <simon.luppescu@gmail.com>
2022-01-23 07:52:39 -0800

    Show letters left

--- game.rb
+++ game.rb
@@ -2,6 +2,7 @@ require "set"
 
 def run
   answer = decide_answer
+  letters = build_letters
 
   while true
     print "What is your guess? "
@@ -9,9 +10,10 @@ def run
 
     if guess.length != 5
       puts "INVALID GUESS. NEEDS TO BE 5 LETTERS"
+      continue
     end
 
-    if evaluate_guess(guess, answer)
+    if evaluate_guess(guess, answer, letters)
       break
     end
   end
@@ -21,7 +23,28 @@ def decide_answer
   word_list.sample
 end
 
-def evaluate_guess(guess, answer)
+def build_letters
+  final = {}
+  letters = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
+  letters.each do |letter|
+    final[letter] = true
+  end
+
+  return final
+end
+
+def print_letters(letters)
+  print "Letters left: "
+
+  poo = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
+  poo.each do |p|
+    print letters[p] ? p : "_"
+  end
+
+  print "\n"
+end
+
+def evaluate_guess(guess, answer, letters)
   if guess == answer
     puts "YOU WIN!!!"
     return true
@@ -39,10 +62,13 @@ def evaluate_guess(guess, answer)
   5.times do |i|
     if answer_letters.include?(guess[i])
       evaluation[i] = :A if evaluation[i] != :O
+    else
+      letters[guess[i]] = false
     end
   end
 
   puts evaluation.inspect
+  print_letters(letters)
   return false
 end
 
